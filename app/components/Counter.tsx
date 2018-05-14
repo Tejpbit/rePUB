00516ef59bd2a9205feb6b5a70ffafa34b81d366
André Samuelsson <andresamuelsson94@gmail.com>
2018-05-14 19:03:53 +0200

    Add tslint. Prettier some files

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -1,20 +1,26 @@
-import * as React from 'react';
-import { RouteComponentProps } from 'react-router';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { RouteComponentProps } from "react-router";
+import { Link } from "react-router-dom";
 
-let styles = require('./Counter.scss');
+let styles = require("./Counter.scss");
 
 export interface IProps extends RouteComponentProps<any> {
-  increment(): void,
-  incrementIfOdd(): void,
-  incrementAsync(): void,
-  decrement(): void,
-  counter: number
+  increment(): void;
+  incrementIfOdd(): void;
+  incrementAsync(): void;
+  decrement(): void;
+  counter: number;
 }
 
 export class Counter extends React.Component<IProps> {
   render() {
-    const { increment, incrementIfOdd, incrementAsync, decrement, counter } = this.props;
+    const {
+      increment,
+      incrementIfOdd,
+      incrementAsync,
+      decrement,
+      counter
+    } = this.props;
     return (
       <div>
         <div className={styles.backButton} data-tid="backButton">
@@ -32,8 +38,20 @@ export class Counter extends React.Component<IProps> {
           <button className={styles.btn} onClick={decrement} data-tclass="btn">
             <i className="fa fa-minus" />
           </button>
-          <button className={styles.btn} onClick={incrementIfOdd} data-tclass="btn">odd</button>
-          <button className={styles.btn} onClick={() => incrementAsync()} data-tclass="btn">async</button>
+          <button
+            className={styles.btn}
+            onClick={incrementIfOdd}
+            data-tclass="btn"
+          >
+            odd
+          </button>
+          <button
+            className={styles.btn}
+            onClick={() => incrementAsync()}
+            data-tclass="btn"
+          >
+            async
+          </button>
         </div>
       </div>
     );
--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { Link } from "react-router-dom";
 
-let styles = require('./Home.scss');
+const styles = require("./Home.scss");
 
 export default class Home extends React.Component {
   render() {
