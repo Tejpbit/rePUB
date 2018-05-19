f4cc35f01fe4e2c9d103d6a5cc31f04bd9e51802
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2018-05-19 11:26:11 +0200

    Initial styling of text components

--- app/components/Text.tsx
+++ app/components/Text.tsx
@@ -42,9 +42,26 @@ export default class Text extends React.Component<Props, any> {
 }
 
 const TextStyle = styled.div`
-    background: white;
     position: absolute;
     z-index: 3;
-    border: solid;
+    margin-left: 20px;
+    padding-left: 10px;
     border-radius: 12px;
+    box-shadow: 4px 4px 5px 0px rgba(50, 50, 50, 0.75);
+    background: #e3f4e3;
+
+    &:after {
+	content: '';
+	position: absolute;
+	left: 0;
+	top: 50%;
+	width: 0;
+	height: 0;
+	border: 20px solid transparent;
+	border-right-color: #e3f4e3;
+	border-left: 0;
+	border-top: 0;
+	margin-top: -10px;
+	margin-left: -20px;
+}
 `;
